b26ec0636da545094858739dd01f30afc0ce95e5
David Brownell <db@DavidBrownell.com>
2020-10-12 18:42:10 -0700

    Working change for JsonApiRestPlugin

--- src/HttpGenerator/Plugin.py
+++ src/HttpGenerator/Plugin.py
@@ -17,15 +17,21 @@
 
 import hashlib
 import os
+import pickle
 import re
 import shutil
+import sys
 import textwrap
+
+from collections import OrderedDict
 
 import six
 
 import CommonEnvironment
+from CommonEnvironment.CallOnExit import CallOnExit
 from CommonEnvironment import FileSystem
 from CommonEnvironment import Interface
+from CommonEnvironment import Process
 from CommonEnvironment.Shell.All import CurrentShell
 from CommonEnvironment import StringHelpers
 
@@ -240,3 +246,184 @@ class Plugin(PluginBase):
             shutil.copyfile(temp_filename, simple_schema_filename)
 
         return simple_schema_filename
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def _CompilePickledSimpleSchemaContent(temp_dir, simple_schema_filename, output_stream, verbose):
+        return Process.Execute(
+            '"{script}" Generate Pickle http_schema "{output_dir}" "/input={input_filename}" "/output_data_filename_prefix=Pickle"{verbose}'.format(
+                script=CurrentShell.CreateScriptName("SimpleSchemaGenerator"),
+                output_dir=temp_dir,
+                input_filename=simple_schema_filename,
+                verbose=" /verbose" if verbose else "",
+            ),
+            output_stream,
+        )
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def _ExtractPickledSimpleSchemaContent(roots, temp_dir):
+        # Load the pickled content
+        pickle_path_filename = os.path.join(temp_dir, "http_schema.path")
+        assert os.path.isfile(pickle_path_filename), pickle_path_filename
+
+        pickle_filename = os.path.join(temp_dir, "http_schema.pickle")
+        assert os.path.isfile(pickle_filename), pickle_filename
+
+        with open(pickle_path_filename) as f:
+            pickle_path = f.read().strip()
+            assert os.path.isdir(pickle_path), pickle_path
+
+        sys.path.insert(0, pickle_path)
+        with CallOnExit(lambda: sys.path.pop(0)):
+            with open(pickle_filename, "rb") as f:
+                content = pickle.load(f)
+
+        # Extract the content
+
+        # ----------------------------------------------------------------------
+        def FindElement(content, content_name):
+            for item in content:
+                if item.Name == content_name:
+                    return item
+
+            return None
+
+        # ----------------------------------------------------------------------
+        def GetTypeInfo(element):
+            if len(element.TypeInfo.Items) != 1:
+                raise Exception("Multiple values were found '{}'".format(list(six.iterkeys(element.TypeInfo.Items))))
+
+            return next(six.itervalues(element.TypeInfo.Items))
+
+        # ----------------------------------------------------------------------
+        def ProcessEndpoint(content, endpoint, endpoint_index, name_prefix):
+            endpoint_name = "endpoint_{}".format(endpoint_index)
+
+            content = FindElement(content, endpoint_name)
+            if content is None:
+                return
+
+            for variable_index, variable in enumerate(endpoint.variables):
+                element = FindElement(content.Children, "variable_{}".format(variable_index))
+                assert element
+
+                variable.simple_schema = {
+                    "string" : variable.simple_schema,
+                    "element" : element,
+                    "type_info" : GetTypeInfo(element),
+                }
+
+            for method_index, method in enumerate(endpoint.methods):
+                method_name = "method_{}".format(method_index)
+
+                method_element = FindElement(content.Children, method_name)
+                if method_element is None:
+                    continue
+
+                for request_index, request in enumerate(method.requests):
+                    request_name = "request_{}".format(request_index)
+
+                    request_element = FindElement(method_element.Children, request_name)
+                    if request_element is None:
+                        continue
+
+                    for prefix, items in [
+                        ("header_", request.headers),
+                        ("query_", request.query_items),
+                        ("form_", request.form_items),
+                    ]:
+                        for item_index, item in enumerate(items):
+                            element = FindElement(request_element.Children, "{}{}".format(prefix, item_index))
+                            assert element
+
+                            item.simple_schema = {
+                                "string" : item.simple_schema,
+                                "element" : element,
+                                "type_info" : GetTypeInfo(element),
+                            }
+
+                    if request.body:
+                        element = FindElement(request_element.Children, "body")
+                        assert element
+
+                        request.body.simple_schema = {
+                            "string" : request.body.simple_schema,
+                            "element" : element,
+                            "type_info" : GetTypeInfo(element),
+                        }
+
+                for response_index, response in enumerate(method.responses):
+                    response_name = "response_{}".format(response_index)
+
+                    response_element = FindElement(method_element.Children, response_name)
+                    if response_element is None:
+                        continue
+
+                    for content_type_index, content_type in enumerate(response.contents):
+                        content_name = "content_{}".format(content_type_index)
+
+                        content_type_element = FindElement(response_element.Children, content_name)
+                        if content_type_element is None:
+                            continue
+
+                        for prefix, items in [
+                            ("header_", content_type.headers),
+                        ]:
+                            for item_index, item in enumerate(items):
+                                item_name = "{}{}".format(prefix, item_index)
+
+                                element = FindElement(content_type_element.Children, item_name)
+                                assert element
+
+                                item.simple_schema = {
+                                    "string" : item.simple_schema,
+                                    "element" : element,
+                                    "type_info" : GetTypeInfo(element),
+                                }
+
+                        if content_type.body:
+                            element = FindElement(content_type_element.Children, "body")
+                            assert element
+
+                            content_type.body.simple_schema = {
+                                "string" : content_type.body.simple_schema,
+                                "element" : element,
+                                "type_info" : GetTypeInfo(element),
+                            }
+
+            for child_index, child in enumerate(endpoint.children):
+                ProcessEndpoint(content.Children, child, child_index, "{}{}_".format(name_prefix, endpoint_name))
+
+        # ----------------------------------------------------------------------
+
+        # Extract the global schema
+        global_simple_schema_index = 0
+
+        for root in six.itervalues(roots):
+            if not root.simple_schema_content:
+                continue
+
+            starting_global_simple_schema_index = global_simple_schema_index
+
+            while global_simple_schema_index < len(content):
+                if content[global_simple_schema_index].Name.startswith("simple_schema_delimiter_"):
+                    break
+
+                global_simple_schema_index += 1
+
+            root.simple_schema_content = {
+                "string" : root.simple_schema_content,
+                "elements" : content[starting_global_simple_schema_index : global_simple_schema_index],
+            }
+
+            # Move beyond the delimiter
+            global_simple_schema_index += 1
+
+        # Extract the endpoints
+        endpoint_index = 0
+
+        for root in six.itervalues(roots):
+            for endpoint in root.endpoints:
+                ProcessEndpoint(content, endpoint, endpoint_index, "")
+                endpoint_index += 1
